40e74b7d0ed4e1cb764abcf2d20cc01daef53315
Anton <anton@teamaton.com>
2012-06-13 17:28:23 +0200

    DB erstellt, BlogPostsController

---
+++ Source/antontelle/Controllers/BlogPostsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace antontelle.Controllers
+{
+    public class BlogPostsController : Controller
+    {
+        //
+        // GET: /BlogPosts/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
