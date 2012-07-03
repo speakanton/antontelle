2ac7f9145eebdf4aa318a76c464f413f11dfa727
Anton <anton@teamaton.com>
2012-07-03 18:10:04 +0200

    Validation

---
+++ Source/antontelle/Scripts/CustomValidationRules.js
@@ -0,0 +1,25 @@
+﻿Sys.Mvc.ValidatorRegistry.validators["excludedwords"] = function (rule) {
+	var words = rule.ValidationParameters.words;
+	var message = rule.ErrorMessage;
+
+	return function (value) {
+		for (var i = 0; i < words.length; i++) {
+			if (value.indexOf(words[i]) >= 0)
+				return message.replace("{0}", words[i]);
+		}
+		return true;
+	}
+}
+
+//jQuery.validator.unobtrusive.adapters.add("excludedwords", function (rule) {
+//	var message = rule.ErrorMessage;
+//	var words = rule.ValidationParameters.words;
+
+//	return function (value) {
+//		for (var i = 0; i < words.length; i++) {
+//			if (value.indexOf(words[i]) >= 0)
+//				return message.replace("{0}", words[i]);
+//		}
+//		return true;
+//	}
+//});
