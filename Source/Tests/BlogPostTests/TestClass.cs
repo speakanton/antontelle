1d32c871a2d2609a0078a18f24f341ed0a06368b
Anton <anton@teamaton.com>
2012-07-31 15:15:22 +0200

    first tests, testing controller

---
+++ Source/Tests/BlogPostTests/TestClass.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using antontelle.Models;
+using antontelle.Controllers;
+using NUnit.Framework;
+
+namespace Tests.BlogPostTests
+{
+	[TestFixture]
+	public class TestClass
+	{
+		[Test]
+		public void Test1()
+		{
+			var blogPosts = new List<BlogPost> {new BlogPost() {Category = Category.Futsal}};
+
+			var summerizer = new Summerizer(blogPosts);
+			var result = summerizer.GetNumberOfPosts(Category.Futsal);
+
+			Assert.AreEqual(result, 1);
+		}
+
+		[Test]
+		public void TestController()
+		{
+			var controller = new BlogPostsController(new BlogPostService());
+			var result = controller.Index() as ViewResult;
+			var model = result.Model as IEnumerable<BlogPost>;
+
+			Assert.AreEqual(17, model.Count());
+		}
+	}
+}
---
+++ Source/antontelle/Models/Summerizer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace antontelle.Models
+{
+	public class Summerizer
+	{
+		private readonly List<BlogPost> BlogPosts;
+
+		public Summerizer(List<BlogPost> blogPosts)
+		{
+			BlogPosts = blogPosts;
+		}
+
+		public int GetNumberOfPosts(Category category)
+		{
+			return BlogPosts.Where(post => post.Category == category).Count();
+		}
+	}
+}
